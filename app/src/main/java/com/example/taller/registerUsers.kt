d5d7b8e95d2c5680c42a6220d1bbf8ab3d4c517b
Sebastian Mosquera <sebastianmosquera1075@gmail.com>
2020-05-24 21:45:57 -0500

    algunos cambios

---
+++ app/src/main/java/com/example/taller/registerUsers.kt
@@ -0,0 +1,12 @@
+package com.example.taller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registerUsers : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_users)
+    }
+}
--- app/src/main/java/com/example/taller/ui/agregar/agregarViewModel.kt
+++ app/src/main/java/com/example/taller/ui/agregar/agregarViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.taller.ui.gallery
+package com.example.taller.ui.agregar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class agregarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
