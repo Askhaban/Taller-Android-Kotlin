b0a1616286a879e400031c7a30d5b08a2c9a77fe
Sebastian Mosquera <sebastianmosquera1075@gmail.com>
2020-05-25 19:57:38 -0500

    cree un objeto kotlin para insertar las ordenes de trabajo a la bd

---
+++ app/src/main/java/com/example/taller/model/ordenTrabajo.kt
@@ -0,0 +1,10 @@
+package com.example.taller.model
+
+class ordenTrabajo(
+    var placa:String="NN",
+    var moto:String,
+    var nombrecliente:String,
+    var celular:String="",
+    var detalle:String,
+    var mo:String=""
+)
